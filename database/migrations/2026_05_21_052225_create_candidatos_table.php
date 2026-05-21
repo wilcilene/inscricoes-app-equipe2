2f5512bacc48fcb19a3daa35db09f5523c2ffcc5
Daniel-Trentin-Mendes <danieltrentinmendes@gmail.com>
2026-05-21 02:29:30 -0300

    Criação da tabela candidato

---
+++ database/migrations/2026_05_21_052225_create_candidatos_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('candidato', function (Blueprint $table) {
+
+            $table->id();
+
+            $table->string('cpf',255);
+
+            $table->date('data_nascimento');
+
+            $table->unsignedBigInteger('usuario_id');
+
+            $table->foreign('usuario_id')
+                ->references('id')
+                ->on('tabela_usuarios');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('candidato');
+    }
+};
