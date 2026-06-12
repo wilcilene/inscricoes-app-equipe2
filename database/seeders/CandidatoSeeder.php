d73c6d5acc0d60cac2ae2724fa6757005704507d
Kauan Felipe Scheidt <scheidtkauan8@gmail.com>
2026-06-11 21:38:14 -0300

    Ajustes no Seeder e Factory

--- database/seeders/CandidatoSeeder.php
+++ database/seeders/CandidatoSeeder.php
@@ -11,42 +11,7 @@ class CandidatoSeeder extends Seeder
     //mudar para ficar no modelo como o editalseeder, para fazer um exemplo
     public function run(): void
     {
-        $candidatos=[
-            [
-             'cpf'=>'12345678900', 
-             'data_nascimento'=>'2003/06/28', 
-             'nome_social'=>'Jorge', 
-             'genero'=>'Masculino', 
-             'naturalidade'=>'SP', 
-             'mae'=>'Katrina Silva', 
-             'cep'=>'99282580', 
-             'logradouro'=>'rua das flores', 
-             'numero'=>'120', 
-             'complemento'=>'nao tem', 
-             'bairro'=>'boa vista', 
-             'estado'=>'SP', 
-             'telefone'=>'11988532769', 
-             'cidade'=>'sao sebastiao', 
-             'usuario_id'=> 1,
-            ],
-           [
-             'cpf'=>'00000232130', 
-             'data_nascimento'=>'2003/01/30', 
-             'nome_social'=>'Maria', 
-             'genero'=>'Feminino', 
-             'naturalidade'=>'SC', 
-             'mae'=>'Lurdes Souza', 
-             'cep'=>'00225801', 
-             'logradouro'=>'rua das margaridas', 
-             'numero'=>'154', 
-             'complemento'=>'nao tem', 
-             'bairro'=>'Centro', 
-             'estado'=>'SC', 
-             'telefone'=>'11988115352', 
-             'cidade'=>'sao bernardo', 
-             'usuario_id'=> 2,
-           ],
-        ];
+        $candidatos= Candidato::factory()->make()->toArray();
 
         foreach ($candidatos as $candidato) {
             Candidato::create($candidato);
