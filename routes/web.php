9e706458d820b385d6725fc0b2fc2be611328d24
Vitor Nogueira de Souza <vitornogueiradesouza01@gmail.com>
2026-06-13 14:07:47 -0300

    correcao de duplicacao

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Http\Controllers\EditalController;
-
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EditalController;
@@ -17,22 +15,37 @@ Route::get("/dashboard", function () {
     ->middleware(["auth", "verified"])
     ->name("dashboard");
 
-    Route::get("/inicio", [EditalController::class, "index"]);
-   
-Route::get("/inicio", [EditalController::class, "index"]);
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+//Route::get("/inicio", [EditalController::class, "index"]);
+Route::get("/dashboard", function () {
+    return view("dashboard");
+})
+    ->middleware(["auth", "verified"])
+    ->name("dashboard");
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::middleware("auth")->group(function () {
+    Route::get("/profile", [ProfileController::class, "edit"])->name(
+        "profile.edit",
+    );
+    Route::patch("/profile", [ProfileController::class, "update"])->name(
+        "profile.update",
+    );
+    Route::delete("/profile", [ProfileController::class, "destroy"])->name(
+        "profile.destroy",
+    );
+    Route::get("/inicio", [EditalController::class, "index"])->name("inicio"); //deixar aqui
+    Route::delete("/edital/{id}", [
+        EditalController::class,
+        "removerEdital",
+    ])->name("edital.remover");
 });
 
 //temporario henrique
-Route::get('/edital/cadastrar', [EditalController::class, 'create'])->name('edital.formulario');
-Route::post('/edital/cadastrar', [EditalController::class, 'store'])->name('edital.store');
+Route::get("/edital/cadastrar", [EditalController::class, "create"])->name(
+    "edital.formulario",
+);
+Route::post("/edital/cadastrar", [EditalController::class, "store"])->name(
+    "edital.store",
+);
 
 Route::get("/interna", function () {
     return view("layouts.interna");
@@ -42,10 +55,18 @@ Route::get("/externa", function () {
     return view("autenticacao.login");
 })->name("login");
 
-Route::get('/candidato/{id}', [InscricaoController::class, 'show'])->name('inscricoes.show');
+Route::get("/candidato/{id}", [InscricaoController::class, "show"])->name(
+    "inscricoes.show",
+);
 
 //Route::get("/inicio", [EditalController::class, "index"])->name("inicio");
 Route::delete("/edital/{id}", [EditalController::class, "removerEdital"])->name(
     "edital.remover",
 );
+Route::get("/teste", function () {
+    //login nao tava funcionando por isso essa parte(retirar depois)
+    Auth::loginUsingId(1);
+    return redirect()->route("inicio");
+})->name("login");
+
 require __DIR__ . "/auth.php";
